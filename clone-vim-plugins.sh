d1347d62f77ebc84e44cf08b6ae88a49a2f9904a
Ryan C. Durham <ryan@stagerightlabs.com>
2019-12-20 13:25:26 -0800

    Fix directory existance checks in vim plugin script

--- clone-vim-plugins.sh
+++ clone-vim-plugins.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d ~/.vim/bundle ]
+then
+	mkdir ~/.vim/bundle
 fi
 
-if [ ! -d "~/.vim/bundle/ctrlp.vim" ]; then
-    git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+if [ ! -d ~/.vim/bundle/Vundle.vim ] 
+then
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+if [ ! -d ~/.vim/bundle/ctrlp.vim ]
+then
+	git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 fi
 
 
