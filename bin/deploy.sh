b60bf78f92265f46e005d8cfc54ecaea1b303482
Ryan C. Durham <ryan@stagerightlabs.com>
2019-12-20 13:24:54 -0800

    Fix typos in dotfile deployment script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,15 +9,21 @@ cp $wd/dotfiles/.bash_profile ~/.bash_profile
 
 # Bash RC
 echo "cp ~/.bashrc"
-cp $wd/dotfiles/.bashrc ~/.bashr
+cp $wd/dotfiles/.bashrc ~/.bashrc
 
 # Profile
 echo "cp ~/.profile"
 cp $wd/dotfiles/.profile ~/.profile
 
+# Ensure the ~/.vim directory exists
+if [ ! -d ~/.vim ]
+then
+	mkdir ~/.vim	
+fi
+
 # Vim RC
 echo "cp ~/.vimrc"
-cp $wd/dotfiles/.vimrc ~/.vimr
+cp $wd/dotfiles/.vimrc ~/.vimrc
 
 # Vim Plugins List
 echo "cp ~/.vim/plugins.vim"
