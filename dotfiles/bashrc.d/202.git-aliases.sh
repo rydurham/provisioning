1368e7642549cc047681c3b1942bf133b5808349
Ryan Durham <ryan@stagerightlabs.com>
2020-01-03 09:23:37 -0800

    Add git branch cleanup function

--- dotfiles/bashrc.d/202.git-aliases.sh
+++ dotfiles/bashrc.d/202.git-aliases.sh
@@ -2,5 +2,12 @@ list_all_branches() {
     for k in `git branch -a | perl -pe 's/^..(.*?)( ->.*)?$/\1/'`; do echo -e `git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k -- | head -n 1`\\t$k; done | sort -r
 }
 
+git_purge() {
+    # https://stevenharman.net/git-clean-delete-already-merged-branches
+    git checkout dev
+    git branch --merged dev | grep -v "\* dev" | xargs -n 1 git branch -d
+}
+
 alias gs='git status --short --branch'
 alias gba=list_all_branches
+alias gp=git_purge
