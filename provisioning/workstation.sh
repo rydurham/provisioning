30da2f3a8d2f1829968e85976516268c77f69f7a
Ryan C. Durham <ryan@stagerightlabs.com>
2020-04-24 11:39:59 -0700

    Update workstation script

--- provisioning/workstation.sh
+++ provisioning/workstation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 /bin/bash ./install-first.sh
+/bin/bash ./configure-git.sh
+/bin/bash ./install-gh.sh
+/bin/bash ./install-tmux.sh
+/bin/bash ./install-vim-plugins.sh
+/bin/bash ./install-postgres.sh
+/bin/bash ./install-php.sh
+/bin/bash ./install-elixir.sh
+/bin/bash ./configure-autocomplete.sh
