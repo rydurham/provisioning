4ae4cb31478cbca974ab54efd7b0d738640ac595
Ryan C. Durham <ryan@stagerightlabs.com>
2020-04-28 11:42:04 -0700

    Tweak php7.3 installation script

--- scripts/install-php7.3.sh
+++ scripts/install-php7.3.sh
@@ -3,9 +3,13 @@
 # PHP
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
-sudo apt install -y php7.3 php7.3-fpm php7.3-cli php7.3-common php7.3-mbstring php7.3-gd php7.3-intl php7.3-xml php7.3-pgsql php7.3-sqlite php7.3-zip php7.3-curl
+sudo apt install -y php7.3 php7.3-fpm php7.3-cli php7.3-common php7.3-mbstring php7.3-gd php7.3-intl php7.3-xml php7.3-pgsql php7.3-sqlite3 php7.3-zip php7.3-curl
 
 # Composer
+if [ ! -d ~/.composer ]
+then
+    mkdir ~/.composer
+fi
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 sudo chown -R ryan ~/.composer/
 
