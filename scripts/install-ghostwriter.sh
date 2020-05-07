a74228ac65115001e27f3f5cea01b4b801c8bcb1
Ryan C. Durham <ryan@stagerightlabs.com>
2020-05-07 15:22:53 -0700

    add ghostwriter installation script

---
+++ scripts/install-ghostwriter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://sourcedigit.com/19270-install-ghostwriter-1-3-2-distraction-free-text-editor-on-ubuntu/
+
+sudo add-apt-repository ppa:wereturtle/ppa
+sudo apt-get update
+sudo apt-get install ghostwriter
