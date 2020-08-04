c457302a9eab180660ecd97016dca52c79d4d074
Ryan C. Durham <ryan@stagerightlabs.com>
2020-08-04 16:18:17 -0700

    Add syncthing installation script

---
+++ scripts/install-syncthing.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Add the release PGP keys:
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+
+# Add the "stable" channel to your APT sources:
+echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+# Update and install syncthing:
+sudo apt update
+sudo apt install apt-transport-https syncthing
