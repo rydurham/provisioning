07e2a240afcee5ee0bc7a5bb1d703919cc4faf4c
Ryan C. Durham <ryan@stagerightlabs.com>
2020-08-08 17:02:17 -0700

    Add systemd service configuration to syncthing installation script

--- scripts/install-syncthing.sh
+++ scripts/install-syncthing.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Capture our current working directory
+wd=$( pwd )
+
 # Add the release PGP keys:
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 
@@ -9,3 +12,13 @@ echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sourc
 # Update and install syncthing:
 sudo apt update
 sudo apt install apt-transport-https syncthing
+
+# Configure systemd service
+if [ ! -d ~/.config/systemd/user ]
+then
+    mkdir -p ~/.config/systemd/user
+fi
+
+cp $wd/conf/syncthing.service ~/.config/systemd/user
+systemctl --user enable syncthing.service
+systemctl --user start syncthing.service
