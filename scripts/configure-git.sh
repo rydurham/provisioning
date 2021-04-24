bae922e9a05e7de2cd64e655b6934074ec1f9905
Ryan C. Durham <ryan@stagerightlabs.com>
2021-04-24 07:27:42 -0700

    add note about signing commits in vscode

--- scripts/configure-git.sh
+++ scripts/configure-git.sh
@@ -6,3 +6,8 @@ git config --global user.email ryan@stagerightlabs.com
 git config --global core.editor "vim"
 git config --global push.default simple
 git config --global pull.rebase false
+
+# signing commits in vscode:
+# https://devmount.medium.com/signed-git-commits-in-vs-code-476fb74b8773
+# git config --global user.signingkey [SIGNATURE]
+# git config --global commit.gpgsign true
