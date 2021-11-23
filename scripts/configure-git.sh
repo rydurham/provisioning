9a36822099a338da22a20d548ac2fdeb6c12d500
Ryan C. Durham <ryan@stagerightlabs.com>
2021-11-23 15:22:08 -0800

    Add default branch name to global git config

--- scripts/configure-git.sh
+++ scripts/configure-git.sh
@@ -6,6 +6,7 @@ git config --global user.email ryan@stagerightlabs.com
 git config --global core.editor "vim"
 git config --global push.default simple
 git config --global pull.rebase false
+git config --global init.defaultBranch main
 
 # signing commits in vscode:
 # https://devmount.medium.com/signed-git-commits-in-vs-code-476fb74b8773
