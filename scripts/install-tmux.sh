ac62e719758902ecb867f5f7edd4659595df2ff0
Ryan C. Durham <ryan@stagerightlabs.com>
2022-12-20 14:16:35 -0800

    adjust tmux configuration

--- scripts/install-tmux.sh
+++ scripts/install-tmux.sh
@@ -2,13 +2,13 @@
 
 sudo apt install build-essential libevent-dev libncurses-dev -y
 
-wget https://github.com/tmux/tmux/releases/download/3.0a/tmux-3.0a.tar.gz
+wget https://github.com/tmux/tmux/releases/download/3.3a/tmux-3.3a.tar.gz
 
-tar -zxvf tmux-3.0a.tar.gz
-cd tmux-3.0a
+tar -zxvf tmux-3.3a.tar.gz
+cd tmux-3.3a
 ./configure
 make
 sudo make install
 cd ..
-rm -rf tmux-3.0a
-rm tmux-3.0a.tar.gz
+rm -rf tmux-3.3a
+rm tmux-3.3a.tar.gz
