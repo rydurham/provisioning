f93441acf933b6fa1ddf1bda7726de19b7800825
Ryan C. Durham <ryan@stagerightlabs.com>
2023-06-06 06:16:33 -0700

    Update alacritty installation script

--- scripts/install-alacritty.sh
+++ scripts/install-alacritty.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:mmstick76/alacritty
+sudo add-apt-repository ppa:aslatter/ppa
 sudo apt install alacritty
+# add term info - https://github.com/alacritty/alacritty/issues/3962#issuecomment-862212371
+curl -sSL https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info | tic -x -
